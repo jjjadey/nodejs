e10cf026557993543237a31ed4578b306ff2aa5b
jjjadey <nattakan.premp@gmail.com>
2023-01-10 21:35:54 +0700

    fs module: sync/async for read and write file

---
+++ 10-fs-sync.js
@@ -0,0 +1,18 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+//read files
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+console.log(`Read first.txt: ${first}`);
+console.log(`Read second.txt: ${second}`);
+
+//write file
+writeFileSync(
+  './content/result-sync.txt',
+  `Here is the result (Sync) : ${first}, ${second}`,
+  { flag: 'a' }
+);
+// flag -> https://nodejs.org/api/fs.html#file-system-flags
+// 'a': Open file for appending. The file is created if it does not exist.
+
+console.log('done: write file result-sync.txt');
---
+++ 11-fs-async.js
@@ -0,0 +1,33 @@
+const { readFile, writeFile } = require('fs');
+
+console.log('start');
+//1.read first.txt
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err);
+    return
+  }
+  const first = result;
+
+  //2.read second.txt
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err);
+      return
+    }
+    const second = result;
+
+    //3. write result-async.txt
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result (Async): ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return
+        }
+        console.log('done: write file result-sync.txt');
+      }
+    );
+  });
+});
