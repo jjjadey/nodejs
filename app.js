84193f2daee4408b90b739a7f2cf3401c48de7a1
jjjadey <nattakan.premp@gmail.com>
2023-01-14 13:03:42 +0700

    package.json and install lodash

--- app.js
+++ app.js
@@ -3,3 +3,17 @@
 // require - function to use moduled (CommonJS)
 // module - info about current module (file)
 // process - info about env where the program is being executed
+
+//npm -global command. come with node
+// global dependency - use it in any project
+// npm i -g <packageName>
+
+// package.json - manifest file (stores important info about project and packages)
+// manual approach -> create package.json in root, create properties etc
+// npm init (step by step, press enter or skip)
+// npm init -y (everything default)
+
+const _ = require('lodash');
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
