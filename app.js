d9e3025e92acd49a46720ec102b4919848faa09a
jjjadey <nattakan.premp@gmail.com>
2023-01-26 21:05:12 +0700

    Basic concept of Event loop

--- app.js
+++ app.js
@@ -13,7 +13,35 @@
 // npm init (step by step, press enter or skip)
 // npm init -y (everything default)
 
-const _ = require('lodash');
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+
+// -----------------import module--------------------------------
+// const _ = require('lodash');
+// const items = [1, [2, [3, [4]]]];
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
+// -----------------end import module--------------------------------
+
+
+// ----------------event loop and callbacks--------------------------------
+setTimeout(() => {
+    console.log('first task');
+}, 0);
+
+console.log('second task');
+
+console.time();
+for (let index = 0; index < 10000; index++) {
+    if (index %10 ===0){
+      console.log('.')  
+    } 
+}
+console.timeEnd();
+
+setTimeout(() => {
+    console.log('third task');
+}, 0);
+
+console.log('next task');
+// https://course-api.com/slides/
+// ----------------end event loop and callbacks--------------------------------
+
